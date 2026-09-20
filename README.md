ae28072d74c763571745f26471500202dd4d7bab
kumarakashrana1204-blip <kumarakashrana1204@gmail.com>
2026-09-21 02:34:12 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# ITI-student-hub
+# ITI Student Hub
+
+A mobile-friendly ITI student learning platform.
+
+## Current version
+- Home page
+- Notes / Information / Practicals / Question Papers sections
+- Responsive mobile design
+- Basic navigation UI
+
+## Planned features
+- Student signup/login
+- PDF and image upload
+- Firebase database and storage
+- Admin approval system
+- Search and filters
+- Student profiles
---
+++ app.js
@@ -0,0 +1,3 @@
+document.querySelector('.login-btn').addEventListener('click',()=>alert('Login system will be added in the next step.'));
+document.querySelector('.upload .primary').addEventListener('click',()=>alert('Student upload system will be added after login and Firebase setup.'));
+document.querySelector('.hero .primary').addEventListener('click',()=>document.querySelector('.latest').scrollIntoView({behavior:'smooth'}));
